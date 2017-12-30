f2c17aaa5e697b7a087ca0749ace0c928c9e3686
Elizabeth Harris <elizabeth@Asemios-MacBook-Pro-2.local>
2017-12-30 15:55:31 -0600

    Adds test for a game with 1 spare and updates score function

--- js/bowlingKata1.js
+++ js/bowlingKata1.js
@@ -1,12 +1,23 @@
 class BowlingGame {
     constructor(){
         this.rolls = [];
+        this.currentRoll = 0;
     }
     roll(pins){
-        this.rolls.push(pins);
+        this.rolls[this.currentRoll++] = pins;
     }
     score(){
-        return this.rolls.reduce((agg,x) => agg + x, 0);
+        let score = 0, frameIndex = 0;
+        for(let f=0; f<10; f++){
+            if(this.rolls[frameIndex] + this.rolls[frameIndex+1] == 10){
+                score += 10 + this.rolls[frameIndex + 2];
+                frameIndex += 2;
+            } else {
+                score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+                frameIndex += 2;
+            }
+        }
+        return score;
     }
 }
 
--- tests/testBowlingKata1.js
+++ tests/testBowlingKata1.js
@@ -2,6 +2,12 @@ import { it, describe } from "mocha";
 import { expect } from "chai";
 import {BowlingGame} from "../js/bowlingKata1";
 
+function rollMany(n, pins){
+    for (let r=0; r<n; r++){
+        this.game.roll(pins);
+    }
+}
+
 describe("Bowling Game", () => {
     it("score game of all gutter balls", () => {
         let game = new BowlingGame();
@@ -10,11 +16,23 @@ describe("Bowling Game", () => {
         }
         expect(game.score()).to.equal(0);
     });
+
     it("score game of all 1s", () => {
         let game = new BowlingGame();
         for(let r=0; r<20; r++){
-            game.roll(1)
+            game.roll(1);
         }
         expect(game.score()).to.equal(20);
+    });
+
+    it("scores a game with one spare", () => {
+        let game = new BowlingGame();
+        for(let r=0; r<20; r++){
+            game.roll(5);
+            game.roll(5);
+            game.roll(3);
+            rollMany(this, 17, 0);
+        }
+        expect(game.score()).to.equal(13);
     });
 });
