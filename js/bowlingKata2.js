002dcc9e82dc893810de7e0afb5c17124c06c3cd
Elizabeth Harris <elizabeth@Asemios-MacBook-Pro-2.local>
2018-01-14 14:53:07 -0600

    Adds tests and game code for game with one strike and a perfect game

--- js/bowlingKata2.js
+++ js/bowlingKata2.js
@@ -1,37 +1,73 @@
-class BowlingGame {
+class Frame {
     constructor(){
-        this.frames = [];
-        this._createEmptyFrame();
+        this.rolls = [];
+    
     }
 
-    _createEmptyFrame(){
-        const frame = [];
-        this.frames.push(frame);
-        return frame;
+    isSpare(){
+        return this.rolls.length === 2 && (this.rolls[0] + this.rolls[1] === 10);
     }
 
-    roll(pins){
-        let frame = this.frames[this.frames.length - 1];
-        if(frame.length === 2){
-            frame = this._createEmptyFrame();
+    isStrike(){
+        return this.rolls.length === 1 && this.rolls[0] === 10;
+    }
+
+    isComplete(){
+        if(this.isStrike()){
+            return true;
+        }
+        
+        if(this.rolls.length === 2){
+            return true;
         }
 
-        frame.push(pins);
+        return false;
+    }
+}
+
+class BowlingGame {
+    constructor(){
+        this.frames = [new Frame()];
+
+    }
+
+    roll(pins) {
+        let frame = this.frames[this.frames.length - 1];
+
+        if(frame.isComplete()){
+            frame = new Frame();
+            this.frames.push(frame);
+        }
+
+        frame.rolls.push(pins);
+    }
+
+    frameScore(frameIndex){
+        const frame = this.frames[frameIndex];
+        let frameScore = frame.rolls.reduce((agg, x) => agg + x, 0);
+        
+        if (frame.isSpare()) {
+            const nextFrame = this.frames[frameIndex + 1];
+            frameScore += nextFrame.rolls[0];
+        }
+
+        if (frame.isStrike()) {
+            const nextFrames = this.frames.slice(frameIndex + 1);
+            const nextRolls = nextFrames.reduce((arr, fr) => arr.concat(fr.rolls), []);
+            const nextTwoRolls = nextRolls.slice(0, 2);
+            frameScore += nextTwoRolls.reduce((agg, x) => agg + x, 0);
+        }
+
+        return frameScore;
     }
 
     score(){
         let score = 0;
         for(let frameIndex = 0; frameIndex < 10; frameIndex++){
-            const frame = this.frames[frameIndex];
-            const frameSum = frame.reduce((agg, x) => agg + x, 0);
-
-            score += frameSum;
-
-            if(frameSum === 10){
-                const nextFrame = this.frames[frameIndex + 1];
-                score += nextFrame[0];
-            }
+            let frameScore = this.frameScore(frameIndex);
+            score += frameScore;
         }
+
         return score;
     }
 }
--- tests/testBowlingKata2.js
+++ tests/testBowlingKata2.js
@@ -30,4 +30,25 @@ describe("Bowling Game", () => {
         
         expect(game.score()).to.equal(16);
     });
+
+    it("score game with one strike", () => {
+        let game = new BowlingGame;
+        game.roll(10);
+        game.roll(3);
+        game.roll(3);
+        rollMany(game, 17, 0);
+        expect(game.score()).to.equal(22);
+    });
+
+    it("scores a perfect game", () => {
+        let game = new BowlingGame();
+        rollMany(game, 12, 10);
+        expect(game.score()).to.equal(300);
+    });
+
+    it("intermediate score of strike should be 10", () => {
+        let game = new BowlingGame();
+        game.roll(10);
+        expect(game.score()).to.equal(10);
+    });
 });
