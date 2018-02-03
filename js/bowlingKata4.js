0163fb577bf9c42d8bf467605d8f980e25ddee50
elizabeth-h <eharris@asemio.com>
2018-02-03 16:40:37 -0600

    Thrid and fourth kata sessions

--- js/bowlingKata4.js
+++ js/bowlingKata4.js
@@ -1,75 +1,59 @@
-class Frame {
-    constructor(){
+class Frame{
+    constructor (){
         this.rolls = [];
-    
-    }
-
-    isSpare(){
-        return this.rolls.length === 2 && (this.rolls[0] + this.rolls[1] === 10);
-    }
-
-    isStrike(){
-        return this.rolls.length === 1 && this.rolls[0] === 10;
     }
 
     isComplete(){
-        if(this.isStrike()){
+        if(this.rolls.length === 1 && this.rolls[0] === 10){
             return true;
         }
-        
         if(this.rolls.length === 2){
             return true;
         }
-
         return false;
     }
 }
 
-class BowlingGame {
+class BowlingGame{
     constructor(){
         this.frames = [new Frame()];
 
     }
 
-    roll(pins) {
+    roll(pins){
         let frame = this.frames[this.frames.length - 1];
-
         if(frame.isComplete()){
             frame = new Frame();
             this.frames.push(frame);
         }
-
         frame.rolls.push(pins);
     }
 
     frameScore(frameIndex){
         const frame = this.frames[frameIndex];
         let frameScore = frame.rolls.reduce((agg, x) => agg + x, 0);
-        
-        if (frame.isSpare()) {
+
+        if(frame.rolls.length === 2 && frameScore === 10){
             const nextFrame = this.frames[frameIndex + 1];
             frameScore += nextFrame.rolls[0];
         }
 
-        if (frame.isStrike()) {
+        if(frame.rolls.length === 1 && frameScore === 10){
             const nextFrames = this.frames.slice(frameIndex + 1);
             const nextRolls = nextFrames.reduce((arr, fr) => arr.concat(fr.rolls), []);
-            const nextTwoRolls = nextRolls.slice(0, 2);
+            const nextTwoRolls = nextRolls.slice(0,2);
             frameScore += nextTwoRolls.reduce((agg, x) => agg + x, 0);
         }
-
         return frameScore;
     }
-
     score(){
         let score = 0;
         for(let frameIndex = 0; frameIndex < 10; frameIndex++){
             let frameScore = this.frameScore(frameIndex);
             score += frameScore;
         }
-
         return score;
     }
 }
 
-export {BowlingGame};
+export{BowlingGame};
